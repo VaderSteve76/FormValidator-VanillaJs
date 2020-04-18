abfd1a3bcf22f0bbd766408e41f74dca4a942d2c
VaderSteve76 <alverson.s@yahoo.com>
2020-04-18 16:48:07 -0400

    Functions for checking required fields & length

--- FormValidator/script1.js
+++ FormValidator/script1.js
@@ -11,3 +11,47 @@ function showError(input, message) {
   const small = formControl.querySelector('small');
   small.innerText = message;
 };
+
+// Show success outline
+function showSuccess(input) {
+  const formControl =parentElement;
+  formControl.className = 'form-control success';
+};
+
+// Check email is valid
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
+};
+
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function(input) {
+    if(input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+};
+
+// Check input length
+function checkLength(input, min, max) {
+  if(input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if(input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+};
